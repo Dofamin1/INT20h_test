0d1303e0db1a3a05e3752fd92ae85e7299874a6c
Anton Kovalenko <anton.kovalenko.cf@ukr.net>
2019-02-08 07:35:29 -0800

    created facePlusPlus service

---
+++ server/src/services/facePlusPlus.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+const flickr = require("./flickr");
+const helpers = require("../helpers/helpers.js");
+const asyncForEach = require("async-foreach").forEach;
+
+const apiKey = "x-q8iByG34SuZR3TFIh6UbWLIkSrrsyM";
+const apiSecret = "ymjVxDrcYSh7Z97g630zVm1WfZkfiokw";
+const baseUrl = "https://api-us.faceplusplus.com/facepp/v3/detect?";
+
+const makeRequest = url =>
+  axios
+    .post(url, { responseType: "json" })
+    .then(
+      ({ data }) => console.log(data)
+      // data.stat === "ok" ? data : new Error("Error with Flickr API")
+    )
+    .catch(e => console.log(e));
+
+const getImageUrl = ({ farm, server, id, secret }) =>
+  `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
+
+const analyzePhotos = async photos => {
+  var canCall = false;
+  asyncForEach(photos, function(photo) {
+    const imageUrl = getImageUrl(photos[0]);
+    const facePlusPlusUrl = `${baseUrl}api_key=${apiKey}&api_secret=${apiSecret}&image_url=${imageUrl}`;
+    var done = this.async();
+    setTimeout(() => {
+      done();
+      makeRequest(facePlusPlusUrl);
+    }, 3000);
+  });
+};
+const faceAnalyzer = {
+  getPhotosInfo: () => {
+    flickr
+      .getAllPhotos()
+      .then(response => {
+        response instanceof Error
+          ? console.log(response)
+          : analyzePhotos(response);
+      })
+      .catch(e => console.log(e));
+  }
+};
+
+module.exports = faceAnalyzer;
