b5ec2918d3fc070bf620eab5b1ccec9927aa624d
Anton Kovalenko <anton.kovalenko.cf@ukr.net>
2019-02-08 02:57:04 -0800

    created "helpers" module + moved  logic that filter photos to getAllPhotos func

--- src/components/photoContainer/index.vue
+++ src/components/photoContainer/index.vue
@@ -31,30 +31,15 @@ export default {
       .then(response => (response instanceof Error
         ? this.handleError(response)
         : this.handleData(response)))
-      .catch(e => console.error(e));
+      .catch(e => this.handleError(e));
   },
   methods: {
-    removeDuplicates({ array, prop }) {
-      // TODO: move to helpers folder
-      return array.filter(
-        (obj, pos, arr) => arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos,
-      );
-    },
     handleError(e) {
       // TODO: global error handler
       window.alert(e);
     },
     handleData(data) {
-      const reducer = (accumulator, currentValue) => [
-        ...accumulator,
-        ...currentValue.photos.photo,
-      ];
-      const allPhotos = data.reduce(reducer, []);
-      const photosWithoutDublicates = this.removeDuplicates({
-        array: allPhotos,
-        prop: 'id',
-      });
-      this.photos = photosWithoutDublicates;
+      this.photos = data;
     },
   },
 };
