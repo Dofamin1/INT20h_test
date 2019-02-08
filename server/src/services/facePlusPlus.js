cab4f0078a178620d623bca25b031a79f7d878bf
foobic <foobic@gmail.com>
2019-02-08 19:11:55 +0200

    Correct photo analyzing

--- server/src/services/facePlusPlus.js
+++ server/src/services/facePlusPlus.js
@@ -12,29 +12,27 @@ const makeRequest = url =>
     .post(url, { responseType: "json" })
     .then(
       ({ data }) => console.log(data)
-      // data.stat === "ok" ? data : new Error("Error with Flickr API")
     )
     .catch(e => console.log(e));
 
 const getImageUrl = ({ farm, server, id, secret }) =>
   `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
 
-const analyzePhotos = async photos => {
-  var canCall = false;
-  asyncForEach(photos, function(photo) {
-    const imageUrl = getImageUrl(photos[0]);
-    const facePlusPlusUrl = `${baseUrl}api_key=${apiKey}&api_secret=${apiSecret}&image_url=${imageUrl}`;
-    const done = this.async();
-    setTimeout(() => {
-      done();
+const analyzePhotos = photos => {
+  let curIndex = 0
+  let facePlusPlusUrl, imageUrl;
+  const interval = setInterval(function(){
+      if (curIndex === photos.length) clearInterval(interval); 
+      imageUrl = getImageUrl(photos[curIndex]);
+      facePlusPlusUrl = `${baseUrl}api_key=${apiKey}&api_secret=${apiSecret}&image_url=${imageUrl}`;
       makeRequest(facePlusPlusUrl);
-    }, 1000);
-  });
+      curIndex += 1
+  },1000)
 };
 const faceAnalyzer = {
   getPhotosInfo: () => {
     flickr
-      .getAllPhotos()
+      .getAllPhotos() 
       .then(response => {
         response instanceof Error
           ? console.log(response)
