31c916ff501c4c4e88a4a3997d43ff6f7125a175
Anton Kovalenko <anton.kovalenko.cf@ukr.net>
2019-02-09 02:44:49 -0800

    added vue-masonry package

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
+import {VueMasonryPlugin} from 'vue-masonry';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
@@ -8,6 +9,8 @@ Vue.config.productionTip = false;
 
 
 Vue.use(BootstrapVue);
+Vue.use(VueMasonryPlugin);
+
 
 new Vue({
   render: h => h(App),
