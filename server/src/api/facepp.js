a2a15c4d13cdbfe2d5229c9ef664e0179fbfd952
Anton Kovalenko <anton.kovalenko.cf@ukr.net>
2019-02-09 03:42:24 -0800

    refactored facepp.js

--- server/src/api/facepp.js
+++ server/src/api/facepp.js
@@ -13,51 +13,43 @@ const getMaximumEmotion = (emotions) => {
   return maxEmotionName;
 };
 
-const analyzePhoto = (reqUrl, imageUrl, cb) => {
+const callAfterAnalysis = analyzedData => db.savePhotos(analyzedData);
+const deleteFaces = el => delete el.attributes.emotion;
+
+const analyzePhoto = (reqUrl, imageUrl, callback) => {
   axios
     .post(reqUrl, { responseType: 'json' })
     .then(({ data }) => {
-      const emotionList = data.faces.length === 0 ? [] : data.faces.map(el => el.attributes.emotion);
-      // remove attributes.emotion because it is not needed already
-      data.faces.forEach((el) => {
-        delete el.attributes.emotion;
-      });
+      const emotionList = data.faces.length ? [] : data.faces.map(el => el.attributes.emotion);
+      // data.faces.forEach(deleteFaces);
       analyzedData.push({
         faces: data.faces,
         url: imageUrl,
-        emotions: emotionList.map(el => getMaximumEmotion(el)),
+        emotions: emotionList.map(getMaximumEmotion),
       });
-      cb();
     })
+    .then(callback)
     .catch(e => console.log(e.message, e.response ? e.response.data : ''));
+};
+
+const photoHandler = (photo) => {
+  const params = new url.URLSearchParams({
+    api_key: apiKey,
+    api_secret: apiSecret,
+    image_url: photo.url,
+    return_attributes: 'emotion',
+  });
+  const reqUrl = `${baseUrl}?${params.toString()}`;
+  const done = this.async(); // should be called after async operation is finished
+  const callAfterEachRequest = () => setTimeout(done, config.request.frequency);
+  analyzePhoto(reqUrl, photo.url, callAfterEachRequest);
 };
 
 module.exports = function facepp(db) {
   return {
     analyzePhotos: (photos) => {
-      // Note: next line will be deleted in future. It is for quick testing
-      photos = photos.length > 5 ? photos.slice(0, 5) : photos; // leave only 3 elements
-      asyncForEach(
-        photos,
-        function photoHandler(photo) {
-          // const imageUrl = flickr.composeImageUrl(photo);
-          const params = new url.URLSearchParams({
-            api_key: apiKey,
-            api_secret: apiSecret,
-            image_url: photo.url,
-            return_attributes: 'emotion',
-          });
-          const reqUrl = `${baseUrl}?${params.toString()}`;
-          const done = this.async();
-          // analyze one photo, after each analysis wait `config.facepp.requestFrequency`
-          // and save analyzed data to analyzedData.
-          analyzePhoto(reqUrl, photo.url, () => setTimeout(done, config.request.frequency));
-        },
-        () => {
-          console.log('asdf');
-          db.savePhotos(analyzedData);
-        },
-      ); // when all done save to db
+      photos.slice(5);
+      asyncForEach(photos, photoHandler, callAfterAnalysis);
     },
   };
 };
